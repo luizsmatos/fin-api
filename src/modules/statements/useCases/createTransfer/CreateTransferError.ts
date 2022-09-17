fb65684ad8066efd2d0c859d91d1b7ffd9185f57
Luiz Gustavo <gusstavopdm@gmail.com>
2022-09-17 00:37:18 -0300

    feat(CreateTransferError): adicionando UnableTransfer

--- src/modules/statements/useCases/createTransfer/CreateTransferError.ts
+++ src/modules/statements/useCases/createTransfer/CreateTransferError.ts
@@ -1,6 +1,12 @@
 import { AppError } from "../../../../shared/errors/AppError";
 
 export namespace CreateTransferError {
+  export class UnableTransfer extends AppError {
+    constructor() {
+      super('Unable to transfer to your own account', 400);
+    }
+  }
+
   export class UserNotFound extends AppError {
     constructor() {
       super('User not found', 404);
