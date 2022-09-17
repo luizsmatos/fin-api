8f1928f0b7105319699d3664a7c586d12258191f
Luiz Gustavo <gusstavopdm@gmail.com>
2022-09-17 00:37:36 -0300

    feat(CreateTransferController): adicionando UnableTransfer

--- src/modules/statements/useCases/createTransfer/CreateTransferController.ts
+++ src/modules/statements/useCases/createTransfer/CreateTransferController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
-import { OperationType } from '../../entities/Statement';
+import { OperationType } from "../../entities/Statement";
+import { CreateTransferError } from "./CreateTransferError";
 
 import { CreateTransferUseCase } from "./CreateTransferUseCase";
 
@@ -10,6 +11,9 @@ class CreateTransferController {
     const { id: userReceive_id } = request.params;
     const { amount, description } = request.body;
 
+    if (userReceive_id === userSender_id) {
+      throw new CreateTransferError.UnableTransfer();
+    }
 
     const createStatement = container.resolve(CreateTransferUseCase);
 
